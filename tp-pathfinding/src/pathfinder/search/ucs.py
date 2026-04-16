a2d10d6a90348801cd586d6de55ba78bc6ce5319
aldanasanchezasz <aldanasanchezasz@gmail.com>
2026-04-16 12:39:10 -0300

    UCS hecho (chequear funcionalidad)

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -21,9 +21,51 @@ class UniformCostSearch:
         # Initialize reached with the initial state
         reached = {}
         reached[root.state] = root.cost
-
+        
+        # ------------------------------------------------------------------
+           
         # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        frontier = PriorityQueueFrontier()
+        frontier.add(root)
+
+        # Agrego al diccionario de alcanzados el estado inicial con su costo:
+        reached[root.state] = root.cost
+
+        # Bucle principal:
+        while True:
+
+            # Si la frontera queda vacía, no hay solución:
+            if frontier.is_empty():
+                return NoSolution(reached)
+            
+            # Si aún hay nodos en la frontera, vamos por el de menor prioridad:
+            node = frontier.pop()
+
+            # Si el nodo que saqué de la frontera es el objetivo, retorno:
+            if grid.objective_test(node.state):
+                return Solution(node, reached)
+
+            # Acciones que puedo realizar con dicho nodo:
+            for action in grid.actions(node.state):
+
+                # Resultado que da aplicar cada acción al nodo:
+                result = grid.result(node.state, action)
+
+                # Obtengo el costo de seguir ese camino:
+                cost = node.cost + grid.individual_cost(node.state, action)
+
+                # Evalúo si conviene usar ese camino:
+                if result not in reached or cost < reached[result]:
+                    
+                    # Creo un nuevo nodo (hijo) con el resultado:
+                    new_node = Node('', result, cost, node, action)
+
+                    # Lo agrego a alcanzados:
+                    reached[result] = cost
+
+                    # Lo agrego a la frontera:
+                    frontier.add(new_node, cost)
+        
+        # ------------------------------------------------------------------
 
         return NoSolution(reached)
