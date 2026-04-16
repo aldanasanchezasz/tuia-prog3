a50b325afdb9d719a64c0a00d76200f885f4e91a
aldanasanchezasz <aldanasanchezasz@gmail.com>
2026-04-16 10:55:53 -0300

    DFS hecho (chequear funcionalidad)

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -21,8 +21,44 @@ class DepthFirstSearch:
         # Initialize expanded with the empty dictionary
         expanded = dict()
 
+        #--------------------------------------------------------------------
+
+        # El nodo raíz, ¿tiene estado objetivo?
+        if grid.objective_test(root.state):
+            return Solution(root, expanded)
+
         # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        frontier = StackFrontier()
+        frontier.add(root)
+
+        # Bucle principal:
+        while True:
+
+            # Si la frontera queda vacía, no hay solución:
+            if frontier.is_empty():
+                return NoSolution(expanded)
+            
+            # Tomamos el último nodo agregado:
+            node = frontier.remove()
+
+            # Acciones que puedo realizar con dicho nodo:
+            for action in grid.actions(node.state):
+
+                # Resultado que da aplicar cada acción al nodo:
+                result = grid.result(node.state, action)
+
+                # Creo un nuevo nodo (hijo) con el resultado:
+                new_node = Node('', result, node.cost + 1, node, action) 
+                
+                # Para evitar ciclos (reviso antes de crear hijo):
+                if result not in expanded:
+                    expanded[result] = True # “ya visité este estado”
+                
+                    # Verifico si este nuevo nodo hijo llegó al objetivo:
+                    if grid.objective_test(new_node.state):
+                        return Solution(new_node, expanded)
+                
+                    # En caso de que no lo sea, lo agrego a la frontera:
+                    frontier.add(new_node)
 
         return NoSolution(expanded)
