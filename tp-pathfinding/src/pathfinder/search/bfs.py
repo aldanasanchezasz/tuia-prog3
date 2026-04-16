2fd124524e09af25f09870fc255f59f347bda3af
aldanasanchezasz <aldanasanchezasz@gmail.com>
2026-04-16 00:15:47 -0300

    BFS listo

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -22,8 +22,46 @@ class BreadthFirstSearch:
         reached = {}
         reached[root.state] = True
 
+        # ------------------------------------------------------------------
+
+        # El nodo raíz, ¿tiene estado objetivo?
+        if grid.objective_test(root.state):
+            return Solution(root, reached)
+
         # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        frontier = QueueFrontier()
+        frontier.add(root)
+
+        # Bucle principal:
+        while True:
+
+            # Si la frontera queda vacía, no hay solución:
+            if frontier.is_empty():
+                return NoSolution(reached)
+            
+            # Si aún hay nodos en la frontera, vamos por el primero:
+            node = frontier.remove()
+
+            # Acciones que puedo realizar con dicho nodo:
+            for action in grid.actions(node.state):
+
+                # Resultado que da aplicar cada acción al nodo:
+                result = grid.result(node.state, action)
+
+                # Para evitar ciclos:
+                if result not in reached:
+                    reached[result] = True # “ya visité este estado”
+
+                    # Creo un nuevo nodo (hijo) con el resultado:
+                    new_node = Node('', result, node.cost + 1, node, action)
+
+                    # Verifico si este nuevo nodo hijo llegó al objetivo:
+                    if grid.objective_test(new_node.state):
+                        return Solution(new_node, reached)
+                    
+                    # En caso de que no lo sea, lo agrego a la frontera:
+                    frontier.add(new_node)
+
+        # ------------------------------------------------------------------
 
         return NoSolution(reached)
